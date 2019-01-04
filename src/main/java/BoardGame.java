dd842b737883849ee62ae855fe05a9acb150d822
Benoitleroux7 <benoitleroux7198@gmail.com>
2019-01-04 01:43:09 +0100

    BoardGame terminé et fonctionnel

--- src/main/java/BoardGame.java
+++ src/main/java/BoardGame.java
@@ -12,8 +12,8 @@ public class BoardGame {
 		this.boardGame[4][4] = HalfDomino.TerrainType.CASTLE;
 	}
 	
-	// Verifie que la case à cote du domino n'est pas vide
-	// que ce terrain correspond au terrain de la moitié du domino qui est accole
+	// Verifie que la case a� cote du domino n'est pas vide
+	// que ce terrain correspond au terrain de la moitie du domino qui est accole
 	// que les deux moities du domino sont sur des cases vides
 	// que le plateau reste de la bonne dimension
 	
@@ -52,12 +52,31 @@ public class BoardGame {
 		
 	// Verifie que le plateau reste de la bonne dimension
 	public static boolean checkIfInBounds(HalfDomino.TerrainType[][] boardGamePlayer, Domino domino, int xLeft, int yLeft, int xRight, int yRight){
+		// Parcours le plateau
 		for (int i = 0; i < 9; i++) {
 			for (int j = 0; j < 9; j++) {
-				boardGamePlayer[i][j] = null;
+				if (boardGamePlayer[i][j] != null) {
+					// Obtient l ecart le plus grand entre les dominos selon i
+					int valIntermediaireI = i;
+					for (int i = 8; i > valIntermediaireI; i--) {
+						while (boardGamePlayer[i][j] == null) {
+							int ecartDominoI = i - valIntermediaireI;
+						}
+					}
+					// Obtient l ecart le plus grand entre les dominos selon j
+					int valeurIntermediaireJ = j;
+					for (int j = 8; j > valeurIntermediaireJ; j--) {
+						while (boardGamePlayer[i][j] == null) {
+							int ecartDominoJ = j - valeurIntermediaireJ;
+						}
+					}
+				}
 			}
-		return false;
 		}
+		if (ecartDominoI <= 5 || ecartDominoJ <= 5) {
+			return true;
+		}
+		return false;
 	}
 		
 	public HalfDomino.TerrainType[][] getBoardGame() { return this.boardGame; }
